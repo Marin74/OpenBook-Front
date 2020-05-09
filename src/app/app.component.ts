66c25e10530490eab26741af80c7f24cdee0af90
Marin Dauriat <marin.dauriat@flyerz.fr>
2020-05-09 18:17:29 +0200

    Minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,23 @@ export class AppComponent {
   constructor() {
     // TODO Retirer Firebase à terme
     var firebaseConfig = {
-      apiKey: "",
+      /*apiKey: "",
       authDomain: "",
       databaseURL: "",
       projectId: "",
       storageBucket: "",
       messagingSenderId: "",
       appId: "",
-      measurementId: ""
+      measurementId: ""*/
+
+      apiKey: "AIzaSyAqGM6ue4m5H3FVOCV72YQKi__w-Itu7Lg",
+      authDomain: "bookshelves-31c7a.firebaseapp.com",
+      databaseURL: "https://bookshelves-31c7a.firebaseio.com",
+      projectId: "bookshelves-31c7a",
+      storageBucket: "bookshelves-31c7a.appspot.com",
+      messagingSenderId: "37323982514",
+      appId: "1:37323982514:web:b950dc538b899812286bab",
+      measurementId: "G-SXT3Q9G3QV"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
